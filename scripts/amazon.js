0300d943b300ce2695bea5574d67ef28186d6e26
Kolibra <kkoliba@gmail.com>
2026-06-01 12:05:51 -0700

    Implementing cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -65,5 +65,10 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       cart.push({ quantity: 1, productId });
       console.log(cart);
     }
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
