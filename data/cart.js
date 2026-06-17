5424706dea7c85d9e96d98eba313ba4f1eda36cb
Kolibra <kkoliba@gmail.com>
2026-06-17 13:40:16 -0700

    Shipping Feature Ongoing

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,18 @@
 export let cart = JSON.parse(localStorage.getItem("cart")) || [
-  { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2 },
-  { productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 1 },
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+    deliveryOptionId: 1,
+  },
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+    deliveryOptionId: 2,
+  },
 ];
+
+localStorage.removeItem("cart");
+localStorage.setItem("cart", JSON.stringify(cart));
 
 function calculateCartQuantity() {
   let cartQuantity = 0;
@@ -31,7 +42,7 @@ export function addToCart(productId) {
   if (matchingItem) {
     matchingItem.quantity++;
   } else {
-    cart.push({ quantity: 1, productId });
+    cart.push({ quantity: 1, productId, deliveryOptionId: 1 });
     console.log(cart);
   }
   saveToStorage(cart);
