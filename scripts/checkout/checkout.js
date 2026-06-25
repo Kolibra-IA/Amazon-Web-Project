649c0c18aa023ca9209f302278147f80ada6e7bd
Kolibra <kkoliba@gmail.com>
2026-06-25 16:52:23 -0700

    Code reorganization

--- scripts/checkout/checkout.js
+++ scripts/checkout/checkout.js
@@ -5,12 +5,12 @@ import {
   updateQuantity,
   saveFunctionality,
   updateDeliveryOption,
-} from "../data/cart.js";
-import { products } from "../data/products.js";
-import { formatCurrency } from "./utils/money.js";
+} from "../../data/cart.js";
+import { products } from "../../data/products.js";
+import { formatCurrency } from "../utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions } from "../data/deliveryOptions.js";
+import { deliveryOptions } from "../../data/deliveryOptions.js";
 
 function renderOrderSummary() {
   let cartSummaryHTML = "";
