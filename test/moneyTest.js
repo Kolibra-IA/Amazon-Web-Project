2e52afda91652eb38c018fcc543eaa2375b7c4ba
Kolibra <kkolibra@gmail.com>
2026-07-12 05:16:24 -0700

    Writing Test Cases

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,6 +1,14 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+//Test Case 1:
 if (formatCurrency(2095) === "20.95") {
+  console.log("Passed!");
+} else {
+  console.log("Failed!");
+}
+
+//Test Case 2:
+if (formatCurrency(0) === "0.00") {
   console.log("Passed!");
 } else {
   console.log("Failed!");
