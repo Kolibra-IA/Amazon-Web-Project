e5b9d1050553c47f813991e3133b07f8b289a9cf
Kolibra <kkolibra@gmail.com>
2026-07-12 07:59:14 -0700

    Test suite and cases

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,6 +1,9 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+console.log("Format Currency Test Suite");
+
 //Test Case 1:
+console.log("Conversion from cents to dollars:");
 if (formatCurrency(2095) === "20.95") {
   console.log("Passed!");
 } else {
@@ -8,7 +11,16 @@ if (formatCurrency(2095) === "20.95") {
 }
 
 //Test Case 2:
+console.log("Testing zero (0):");
 if (formatCurrency(0) === "0.00") {
+  console.log("Passed!");
+} else {
+  console.log("Failed!");
+}
+
+//Test Case 3:
+console.log("Rounding up:");
+if (formatCurrency(2000.5) === "20.01") {
   console.log("Passed!");
 } else {
   console.log("Failed!");
