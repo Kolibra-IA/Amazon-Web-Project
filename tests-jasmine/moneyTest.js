e97790b1da6997edc248cfaf7776b6a8c4d1f487
Kolibra <kkolibra@gmail.com>
2026-07-15 07:23:44 -0700

    Writing a test suite via Jasmine

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,7 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe("Test Suite: Format Currency", () => {
+  it("Convert Cents into Dollars", () => {
+    expect(formatCurrency("2095")).toEqual("20.95");
+  });
+});
