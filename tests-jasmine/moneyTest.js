7b6c4275f2333f3186c91cd7c9742c12fd5a6bd7
Kolibra <kkolibra@gmail.com>
2026-07-15 17:15:26 -0700

    Writing test cases.

--- tests-jasmine/moneyTest.js
+++ tests-jasmine/moneyTest.js
@@ -4,4 +4,10 @@ describe("Test Suite: Format Currency", () => {
   it("Convert Cents into Dollars", () => {
     expect(formatCurrency("2095")).toEqual("20.95");
   });
+  it("Works with 0", () => {
+    expect(formatCurrency("0")).toEqual("0.00");
+  });
+  it("Round up to the nearest cent", () => {
+    expect(formatCurrency("2000.5")).toEqual("20.01");
+  });
 });
