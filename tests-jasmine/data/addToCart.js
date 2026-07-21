426516ac6c51221b98a98ee2dfb3b0333d820b7e
Kolibra <kkolibra@gmail.com>
2026-07-21 15:42:27 -0700

    Writing test cases.

--- tests-jasmine/data/addToCart.js
+++ tests-jasmine/data/addToCart.js
@@ -4,6 +4,7 @@ import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
 
 describe("Test Suite: addToCart", () => {
   it("Add an existing product to the cart", () => {
+    spyOn(localStorage, "setItem");
     spyOn(localStorage, "getItem").and.callFake(() => {
       return JSON.stringify([
         {
@@ -16,6 +17,8 @@ describe("Test Suite: addToCart", () => {
     loadFromStorage();
     addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
     expect(cart[0].quantity).toEqual(2);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
   });
   it("Add a new product to the cart", () => {
     spyOn(localStorage, "getItem").and.callFake(() => {
@@ -24,6 +27,7 @@ describe("Test Suite: addToCart", () => {
     loadFromStorage();
     addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
     expect(cart.length).toEqual(1);
+
     /*
     console.log(localStorage.getItem("hello"));
     console.log(localStorage.getItem("cart"));
