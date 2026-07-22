d5eada0cbdbafa09ac6c4ba847594f227732e86a
Kolibra <kkolibra@gmail.com>
2026-07-22 03:42:32 -0700

    Writing test cases.

--- tests-jasmine/checkout/orderSummary.js
+++ tests-jasmine/checkout/orderSummary.js
@@ -1,5 +1,27 @@
-import { renderOrderSummary } from "../../scripts/checkout/orderSummary";
+import {
+  renderOrderSummary,
+  loadFromStorage,
+} from "../../scripts/checkout/orderSummary";
 
 describe("Test suite: renderOrderSummary", () => {
-  it("Display the cart", () => {});
+  it("Display the cart", () => {
+    document.querySelector(".js-test-container").innerHTML =
+      `<div class='js-order-summary></div>`;
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return [
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ];
+    });
+    loadFromStorage();
+    renderOrderSummary();
+  });
 });
