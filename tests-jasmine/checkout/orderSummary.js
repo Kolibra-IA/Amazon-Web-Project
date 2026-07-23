1848957fc6d09358c9a57c490f98d35a4e28c4f7
Kolibra <kkoliba@gmail.com>
2026-07-23 07:20:39 -0700

    Writing test cases.

--- tests-jasmine/checkout/orderSummary.js
+++ tests-jasmine/checkout/orderSummary.js
@@ -1,14 +1,14 @@
-import {
-  renderOrderSummary,
-  loadFromStorage,
-} from "../../scripts/checkout/orderSummary";
+import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
+
+import { loadFromStorage } from "../../data/cart.js";
 
 describe("Test suite: renderOrderSummary", () => {
   it("Display the cart", () => {
+    console.log(document.querySelector(".js-test-container").innerHTML);
     document.querySelector(".js-test-container").innerHTML =
-      `<div class='js-order-summary></div>`;
+      `<div class='js-order-summary'></div>`;
     spyOn(localStorage, "getItem").and.callFake(() => {
-      return [
+      return JSON.stringify([
         {
           productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
           quantity: 2,
@@ -19,7 +19,7 @@ describe("Test suite: renderOrderSummary", () => {
           quantity: 1,
           deliveryOptionId: "2",
         },
-      ];
+      ]);
     });
     loadFromStorage();
     renderOrderSummary();
