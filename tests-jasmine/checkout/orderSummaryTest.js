eb53af1a5484f4372078e156a6aa7b6380a35db9
Kolibra <kkolibra@gmail.com>
2026-07-23 13:27:23 -0700

    Update the name of OrderSummaryTest.js

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 
-import { loadFromStorage } from "../../data/cart.js";
+import { loadFromStorage, cart } from "../../data/cart.js";
 
 describe("Test suite: renderOrderSummary", () => {
   it("Display the cart", () => {
