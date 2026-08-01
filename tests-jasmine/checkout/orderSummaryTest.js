e16a04149fd0c9c8e0228574013a01b5b1368390
Kolibra <kkolibra@gmail.com>
2026-08-01 12:43:57 -0700

    Writing test cases

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -1,43 +1,13 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
-
 import { loadFromStorage, cart } from "../../data/cart.js";
 
-describe("Test suite: renderOrderSummary", () => {
-  it("Display the cart", () => {
-    const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
-    document.querySelector(".js-test-container").innerHTML =
-      `<div class='js-order-summary'></div>`;
-    spyOn(localStorage, "getItem").and.callFake(() => {
-      return JSON.stringify([
-        {
-          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-          quantity: 2,
-          deliveryOptionId: "1",
-        },
-        {
-          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-          quantity: 1,
-          deliveryOptionId: "2",
-        },
-      ]);
-    });
-    loadFromStorage();
-    renderOrderSummary();
-    expect(cart.length).toEqual(
-      document.querySelectorAll(".js-cart-item-container").length,
-    );
+const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
 
-    expect(
-      document.querySelector(`.js-product-quantity-${productId1}`).innerText,
-    ).toContain("Quantity: 2");
-    expect(
-      document.querySelector(`.js-product-quantity-${productId2}`).innerText,
-    ).toContain("Quantity: 1");
-  });
-  it("Remove from cart", () => {
-    const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+describe("Test suite: renderOrderSummary", () => {
+  beforeEach(() => {
+    spyOn(localStorage, "setItem");
+
     document.querySelector(".js-test-container").innerHTML =
       `<div class='js-order-summary'></div> 
       <div class='js-payment-summary'></div>`;
@@ -57,6 +27,17 @@ describe("Test suite: renderOrderSummary", () => {
     });
     loadFromStorage();
     renderOrderSummary();
+  });
+  it("Display the cart", () => {
+    expect(
+      document.querySelector(`.js-product-quantity-${productId1}`).innerText,
+    ).toContain("Quantity: 2");
+    expect(
+      document.querySelector(`.js-product-quantity-${productId2}`).innerText,
+    ).toContain("Quantity: 1");
+    document.querySelector(".js-test-container").innerHTML = "";
+  });
+  it("Remove from cart", () => {
     document.querySelector(`.js-delete-link-${productId1}`).click();
     expect(document.querySelector(`.js-delete-link-${productId1}`)).toEqual(
       null,
@@ -69,5 +50,6 @@ describe("Test suite: renderOrderSummary", () => {
     ).not.toEqual(null);
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
+    document.querySelector(".js-test-container").innerHTML = "";
   });
 });
